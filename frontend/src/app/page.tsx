a7c43385ff4ac6e563c6c4ed7b54003557c34b88
Kileyydev <ivy.kiley@strathmore.edu>
2025-11-15 09:56:44 +0300

    Fix import paths for HeroSection and Footer components to ensure correct module resolution

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import TopNavigation from "./components/TopNavBar";
-import HeroSection from "@/app/components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import CTASection from "./components/CTASection";
-import Footer from "@/app/components/FooterSection";
 import InterfaceSection from "./components/InterfaceSection";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/FooterSection";
+
 
 export default function Home() {
   return (
