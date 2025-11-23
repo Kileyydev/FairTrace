ddb242055a3fc12aac9ecd75e93c22007d030af7
Kileyydev <ivy.kiley@strathmore.edu>
2025-11-23 10:11:06 +0300

    Update CORS settings to allow requests from the Vercel deployment

--- fairtrace-backend/fairtrace_backend/settings.py
+++ fairtrace-backend/fairtrace_backend/settings.py
@@ -163,9 +163,10 @@ SIMPLE_JWT = {
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://fair-trace.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://fair-trace.vercel.app",
+    
 ]
 
 # WEB3 / BLOCKCHAIN SETTINGS
